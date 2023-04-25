cc0a38a9cadc6ad068e7e2c3779e30ec342779e8
Thyago Holanda Monteiro de Lima <106354725+ThyagoHolanda@users.noreply.github.com>
2023-04-24 23:21:33 -0300

    javascript for
    Começei a criar um for para calcular o montante

--- script.js
+++ script.js
@@ -1,22 +1,53 @@
 function calculadora(){
-    //window.alert("Funcionou!!!")
+    window.alert("Inicio!!!")
+
     valorinicial = Number(document.getElementById('valor').value)
     aporte = Number(document.getElementById('aporteM').value)
     juros = Number((document.getElementById('juros').value/12)/100)
-    mes = Number(document.getElementById('mes').value)
+    ano = Number(document.getElementById('ano').value)
     montante = 0
+    res= document.getElementById('res')
+    taxa = []
 
-    for (i = 0; i < mes; i++) {
+    alert(`Valor Inicial: ${valorinicial}`)
+    alert(`Aporte: ${aporte}`)
+    alert(`juros: ${juros}`)
+    alert(`Ano: ${ano}`)
+
+    /**
+    for (i = 0; i < (ano*12); i++) {
         if(i==0){
             montante = (((valorinicial+aporte)*juros))+(valorinicial+aporte)
         }else{
             montante = (((montante+aporte)*juros))+(montante+aporte)
         }
     }
-
-
+    */
     
-    res= document.getElementById('res')
-    res.innerHTML = montante.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    for (i = 0; i < (ano*12); i++) {
+        if(i==0){
+            taxa[i] = ((valorinicial+aporte)*(juros+1)).toFixed(2)
+        }else{
+            taxa[i] = ((taxa[i-1]+aporte)*(juros+1)).toFixed(2)
+        }
+    }
+    soma = 0
+    for (i=0; i < taxa.length; i++){
+        montante += Number(taxa[i])
+    }
+
+    res.innerHTML = montante
+    //totalinvestido = valorinicial+(aporte*(ano*12))
+    
+    
+    
+    /**
+    res.innerHTML = `Total Investido: R$ ${totalinvestido.toFixed(2)}
+    <br>Total de Juros: ${(montante-totalinvestido).toFixed(2)}
+    <br>Montante: R$ ${montante.toFixed(2)}`
+    
+    Costumizar numero em Real
+    .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    **/
 
 }
