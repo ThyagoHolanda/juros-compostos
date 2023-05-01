90d86a6caa1c1d29ee137cc3d9d35c98db105f19
Thyago Holanda Monteiro de Lima <106354725+ThyagoHolanda@users.noreply.github.com>
2023-04-30 23:47:26 -0300

    script funcionando

--- script.js
+++ script.js
@@ -3,56 +3,39 @@ function calculadora(){
 
     valorinicial = Number(document.getElementById('valor').value)
     aporte = Number(document.getElementById('aporteM').value)
-    juros = Number((document.getElementById('juros').value/12)/100)
+    juros = Number(((document.getElementById('juros').value/12)/100)+1)
     ano = Number(document.getElementById('ano').value)
-    montante = 0
     res= document.getElementById('res')
     res.innerHTML = "";
-    taxa = []
+    mes = []
 
 
     alert(`Valor Inicial: ${valorinicial}`)
     alert(`Aporte: ${aporte}`)
     alert(`juros: ${juros}`)
     alert(`Ano: ${ano}`)
-
-    /**
-    for (i = 0; i < (ano*12); i++) {
-        if(i==0){
-            montante = (((valorinicial+aporte)*juros))+(valorinicial+aporte)
-        }else{
-            montante = (((montante+aporte)*juros))+(montante+aporte)
-        }
-    }
-    */
     
     for (i = 0; i < (ano*12); i++) {
         if(i==0){
-            taxa[i] = ((valorinicial+aporte)*(juros+1)).toFixed(2)
+            mes[i] = (valorinicial+aporte)*juros
 
-            res.innerHTML += i + ": " + taxa[i] + "<br>"
-        }else if (i>=1){
-            taxa[i] = ((taxa[i-1]+valorinicial+aporte)*(juros+1)).toFixed(2)
+            res.innerHTML += `<br>Mês ${i+1} -> ${mes[i].toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}<br>`
+        }else {
+            mes[i] = (mes[i-1]+aporte)*juros
 
-            res.innerHTML += i + ": " + taxa[i-1]+taxa[i] + "<br>"
+            res.innerHTML += `Mês ${i+1} -> ${mes[i].toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}<br>`
         }
     }
-    soma = 0
-    for (i=0; i < taxa.length; i++){
-        montante += Number(taxa[i])
-    }
-/*
-    for (n = 0; n<taxa.length; n++) {
-        res.innerHTML += n + ": " + taxa[n] + "<br>"
-    }
-*/
-    res.innerHTML += `Total: ${montante.toFixed(2)}`    
+    res.innerHTML += `<br>Total -> ${mes[(ano*12)-1].toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}<br>`
     
-    /**
-    res.innerHTML = `Total Investido: R$ ${totalinvestido.toFixed(2)}
-    <br>Total de Juros: ${(montante-totalinvestido).toFixed(2)}
-    <br>Montante: R$ ${montante.toFixed(2)}`
-    
+    totalinvestido = valorinicial+(aporte*(ano*12))
+    rendimento = mes[mes.length - 1]-totalinvestido
+    acumulado = mes[mes.length - 1]
+    res.innerHTML += `<br>Total investido = ${totalinvestido.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}<br>`
+    res.innerHTML += `Rendimento = ${rendimento.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}<br>`
+    res.innerHTML += `Acumulado = ${acumulado.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'})}`
+
+    /**    
     Costumizar numero em Real
     .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
     **/
